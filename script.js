3c4d72afd8e1d84ee86e5d59380d51a14e37db63
Aaron Langley <alangley345@gmail.com>
2021-10-18 21:12:50 -0400

    added slash at end of api to ensure route is correct

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
 var update = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-update.open('GET', 'https://api.aaronlangley.net/updateVisitorCount', true)
-
-// Send request
+update.open('GET', 'https://api.aaronlangley.net/updateVisitorCount/', true)
+update.setRequestHeader("Content-Type", "application/json");
 update.send()
