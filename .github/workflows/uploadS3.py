ab2cb806e107772c20a639364be254b872a0a0ae
Aaron Langley <alangley345@gmail.com>
2021-10-19 21:29:41 -0400

    added testfile and modified script to test boto3 call to AWs

--- .github/workflows/uploadS3.py
+++ .github/workflows/uploadS3.py
@@ -1,21 +1,10 @@
-#!/usr/bin/env python3
-
-import pathlib
 import boto3
-
-
-BASE_DIR = pathlib.Path(__file__).parent.resolve()
-
-region = "us-east-2"
-bucket = "resume."
-
-s3_client = boto3.client("s3", region_name="us-east-2")
-
-def upload_files(file_name, bucket, object_name=None, args=None):
-    if object_name is None:
-        object_name = file_name
-
-    s3_client.upload_file(file_name, bucket, object_name, ExtraArgs=args)
-    print(f"'{file_name}' has been uploaded to ' {bucket}")
-
-upload_files(f"{BASE_DIR}/files/demo.txt", bucket)
+ 
+# Initialize interfaces
+#s3Client = boto3.client('s3')
+s3Resource = boto3.resource('s3')
+ 
+# upload_file
+boto3Upload = '' 
+s3Resource.meta.client.upload_file(boto3Upload, 'gpipis-bucket', boto3Upload)
+ 
