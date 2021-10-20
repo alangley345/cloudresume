f3bbb18d408b4d8e907beff7ef54dd9a66650348
Aaron Langley <alangley345@gmail.com>
2021-10-19 21:46:35 -0400

    change script path

--- .github/workflows/uploadS3.py
+++ .github/workflows/uploadS3.py
@@ -2,9 +2,9 @@ import boto3
  
 # Initialize interfaces
 #s3Client = boto3.client('s3')
-s3Resource = boto3.resource('s3')
+s3Resource = boto3.resource('resume.aaronalangley.net')
  
 # upload_file
-boto3Upload = '' 
-s3Resource.meta.client.upload_file(boto3Upload, 'gpipis-bucket', boto3Upload)
+boto3Upload = 'testfile.txt' 
+s3Resource.meta.client.upload_file(boto3Upload,boto3Upload)
  
