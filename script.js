1642692bbf0002be16b9d9bb145d36c398557f74
Aaron Langley <alangley345@gmail.com>
2021-10-20 21:34:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-var update = new XMLHttpRequest()
-update.open('GET', 'https://api.aaronlangley.net/updateVisitorCount/', true)
-update.setRequestHeader("Content-Type", "application/json");
-update.send()
