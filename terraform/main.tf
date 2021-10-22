8b978c9a31048e2aa4b99e57277b27b64bad0828
Aaron Langley <alangley345@gmail.com>
2021-10-21 20:54:56 -0400

    Added terraform files and error.html page

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,12 @@
 #specific provider
 provider "aws" {
-  profile = "default"
   region  = "us-east-2"
 }
 
 #define location of state file
 terraform {
   backend "s3" {
-    bucket = "myterraformcode"
+    bucket = "myterraform-us-east-2"
     key    = "cloudresume-frontend/terraform.tfstate"
     region = "us-east-2"
   }
