a11a0bc4fae79817b5552d2f18a2b94e297ef1a5
Aaron Langley <alangley345@gmail.com>
2021-10-22 22:02:33 -0400

    Added a bunch of Terraform code

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,12 +1,17 @@
-resource "aws_cloudfront_distribution" "gitbook" {
+locals {
+  s3_origin_id = "cloudresumechallenge"
+}
+
+resource "aws_cloudfront_distribution" "resumesite" {
   enabled             = true
   default_root_object = "index.html"
-  aliases             = [resume.aaronlangley.net]
   
+  depends_on = [aws_s3_bucket.site-bucket, aws_acm_certificate.cert]
+
   default_cache_behavior {
     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "aws_s3_bucket.site-bucket"
+    target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
 
@@ -24,12 +29,12 @@ resource "aws_cloudfront_distribution" "gitbook" {
   }
 
   origin {
-    domain_name = "aws_s3_bucket.site-bucket_regional_domain_name"
-    origin_id   = "aws_s3_bucket.site-bucket"
+    domain_name = aws_s3_bucket.site-bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.gitbook.cloudfront_access_identity_path
-    }
+    #s3_origin_config {
+    #  origin_access_identity = aws_cloudfront_origin_access_identity.gitbook.cloudfront_access_identity_path
+    #}
   }
 
   restrictions {
@@ -39,8 +44,8 @@ resource "aws_cloudfront_distribution" "gitbook" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
