71c24e054baec7e6593d56c7333ddb30fe1885f4
Aaron <alangley345@gmail.com>
2021-11-01 09:43:18 -0400

    Trying to get cloudfront to work

--- terraform/cf_resume.tf
+++ terraform/cf_resume.tf
@@ -1,7 +1,8 @@
-​​resource "aws_cloudfront_distribution" "resume" {
+​​resource "aws_cloudfront_distribution" "resume"{
   origin {
     domain_name = "${aws_s3_bucket.resume.bucket_regional_domain_name}"
-    origin_id   = "${local.s3_origin_id}"#   s3_origin_config {
+    origin_id   = "${local.s3_origin_id}"
+    #   s3_origin_config {
     #     origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
     #   }
   }
@@ -15,11 +16,14 @@
   # }#aliases = []default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${local.s3_origin_id}"forwarded_values {
+    target_origin_id = "${local.s3_origin_id}"
+    forwarded_values{
       query_string = falsecookies {
         forward = "none"
       }
-    }viewer_protocol_policy = "allow-all"
+    }
+    
+    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -30,12 +34,14 @@
     path_pattern     = "/content/immutable/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "${local.s3_origin_id}"forwarded_values {
+    target_origin_id = "${local.s3_origin_id}"
+    forwarded_values{
       query_string = false
       headers      = ["Origin"]cookies {
         forward = "none"
       }
     }
+    
     min_ttl                = 0
     default_ttl            = 86400
     max_ttl                = 31536000
