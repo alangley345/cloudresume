775a633366989ad376c888ea8a081601cfe81df9
Aaron <alangley345@gmail.com>
2021-11-08 11:26:53 -0500

    Adding a counter to HTML, variable to JS

--- site/script.js
+++ site/script.js
@@ -2,3 +2,6 @@ var update = new XMLHttpRequest()
 update.open('GET', 'https://api.aaronlangley.net/updateVisitorCount/', true)
 update.setRequestHeader("Content-Type", "application/json");
 update.send()
+
+let count = 1;
+    document.getElementById("count").innerHTML = count;
