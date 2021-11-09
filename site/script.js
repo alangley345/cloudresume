bcfaccf50728feabd5d671fef7818e58aadb1296
Aaron <alangley345@gmail.com>
2021-11-09 10:04:46 -0500

    Added new line to function

--- site/script.js
+++ site/script.js
@@ -8,5 +8,5 @@ update.send()
 function showCount() {
     let visitCount = "1";
     const id    = document.getElementById("visits");
-    id.value    = innerText.visitCount
+    id.write(visitCount)
 }
