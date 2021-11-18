688e7b93702afb5ac90730e11f1ceecc8577aec1
Aaron Langley <alangley345@gmail.com>
2021-11-17 23:04:41 -0500

    Spliting this up claiming the bucket value

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@
 #   value = aws_cloudfront_distribution.resume.id
 #}
 
-output "cloudresume_frontend_secret" {
-    value = aws_iam_access_key.cloudresume_frontend.encrypted_secret
-}
+#output "cloudresume_frontend_secret" {
+#   value = aws_iam_access_key.cloudresume_frontend.encrypted_secret
+#}
 
-output "cloudresume_frontend_ID" {
-   value = aws_iam_user.cloudresume_frontend.id
-}
+#output "cloudresume_frontend_ID" {
+#  value = aws_iam_user.cloudresume_frontend.id
+#}
