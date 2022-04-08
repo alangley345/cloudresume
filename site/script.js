96b53fdd25b0ba06ba856730f4a45dbc942ca12a
Aaron Langley <alangley@gmail.com>
2022-04-08 11:00:30 -0400

    Trying to get API calls to work right

--- site/script.js
+++ site/script.js
@@ -1,15 +1,23 @@
-var update = new XMLHttpRequest()
-update.open('GET', 'https://api.aaronlangley.net/updateVisitorCount/', true)
-update.setRequestHeader("Content-Type", "application/json");
-update.send()
-
+function updateCount() {
+    var update = new XMLHttpRequest()
+    update.open('GET', 'https://api.aaronlangley.net/updateCount/', true)
+    update.setRequestHeader("Content-Type", "application/json");
+    update.send()
+}
 
 //document.getElementById("visits").innerHTML = "1";
 
-function showCount() {
-    visitCount = "to be implemented";
+function getCount() {
+    var get = new XMLHttpRequest()
+    get.open('GET', 'https://api.aaronlangley.net/getCount/', true)
+    get.setRequestHeader("Content-Type", "application/json");
+    response = get.send()
+    const obj = JSON.parse(response)
+    visitCount = obj.visits
+    return visitCount
     //const id    = document.getElementById("visits").innerHTML= visitCount;
-    //id.innerText = new visitCount
+    //id.innerText = new visitCount.visits
 }
 
-showCount()
+updateCount()
+getCount()
