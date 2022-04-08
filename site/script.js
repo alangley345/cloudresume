bae6df822dbf8c155442fa642ac5b5218779a5a9
Aaron Langley <alangley@gmail.com>
2022-04-08 16:19:32 -0400

    Promises, gerrr

--- site/script.js
+++ site/script.js
@@ -1,12 +1,12 @@
 function getCount(url) {
     return fetch(url)
         .then(response =>{return response.json()})
-        .then(data => {return data.resolve(data.body.Item.visits)})
+        .then(data => {return data.resolve(JSON.stringify(data.body.Item.visits))})
 }
 
 function visits() {
     var visitCount;
-    getCount('https://api.aaronlangley.net/getCount').then(data => { return visitCount = String(data)});
+    getCount('https://api.aaronlangley.net/getCount').then(data => { return visitCount = data});
 }
 
 fetch('https://api.aaronlangley.net/updateCount');
