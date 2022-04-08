18382119f6763585efe01faf1f1b5d997af5b40e
Aaron Langley <alangley@gmail.com>
2022-04-08 11:30:19 -0400

    Testing javascript still

--- site/script.js
+++ site/script.js
@@ -2,7 +2,7 @@ function updateCount() {
     var update = new XMLHttpRequest()
     update.open('GET', 'https://api.aaronlangley.net/updateCount', true)
     update.setRequestHeader("Content-Type", "application/json");
-    update.send()
+    update.send();
 }
 
 //document.getElementById("visits").innerHTML = "1";
@@ -11,13 +11,13 @@ function getCount() {
     var get = new XMLHttpRequest()
     get.open('GET', 'https://api.aaronlangley.net/getCount', true)
     get.setRequestHeader("Content-Type", "application/json");
-    response = get.send()
-    const obj = JSON.parse(response)
-    visitCount = obj.visits
-    return visitCount
+    response = get.send();
+    const obj = JSON.parse(response);
+    visitCount = obj.visits;
+    return visitCount;
     //const id    = document.getElementById("visits").innerHTML= visitCount;
     //id.innerText = new visitCount.visits
 }
 
-updateCount()
-getCount()
+updateCount();
+getCount();
