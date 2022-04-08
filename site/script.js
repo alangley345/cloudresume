79395f9b1095dde78a475dc3b74293230a2fae26
Aaron Langley <alangley@gmail.com>
2022-04-08 13:22:51 -0400

    Switching to fetch api

--- site/script.js
+++ site/script.js
@@ -1,19 +1,12 @@
-function updateCount() {
-    fetch('https://api.aaronlangley.net/updateCount')
-}
-
-//document.getElementById("visits").innerHTML = "1";
-
 function getCount() {
     fetch('https://api.aaronlangley.net/getCount')
-    .then(data => {
-    return data.json();
-    })
-    .then(Item => {
-    console.log(Item.visits);
-    });
+        .then(response => response.json())
+        .then(result => {console.log(result)})
 }
 
-updateCount()
-visitCount = getCount()
+fetch('https://api.aaronlangley.net/updateCount');
+getCount();
+
+visitCount = "test";
+
 
