ec454572d9867a6ad97f2deb4a1dfd3df079cde2
Aaron Langley <alangley@gmail.com>
2022-04-08 14:53:39 -0400

    Still get fetch to work

--- site/script.js
+++ site/script.js
@@ -1,11 +1,9 @@
 async function getCount(url) {
     const response = await fetch(url);
     var data       = await response.json()
-    var siteHits = JSON.stringify(data.body.Item.visits)
-    return siteHits
+    return data.body.Item.visits
 }
 
 fetch('https://api.aaronlangley.net/updateCount');
-getCount('https://api.aaronlangley.net/getCount');
 
-var visitCount = getCount('https://api.aaronlangley.net/getCount');
+var visitCount = JSON.stringify(getCount('https://api.aaronlangley.net/getCount'))
