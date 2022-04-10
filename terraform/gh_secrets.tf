6edb66ceb9ad299974935b74f43188bc6f1f5ee9
Aaron Langley <aaron@thinkpad.int.greatcatlab.net>
2022-04-10 09:53:46 -0400

    Added provider for Github

--- terraform/gh_secrets.tf
+++ terraform/gh_secrets.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "resume_distribution" {
-  repository       = "cloudresume"
+  repository       = "cloudresume-frontend"
   secret_name      = "DISTRIBUTION_ID"
   encrypted_value  = "${aws_cloudfront_distribution.resume.id}"
 }
