507ac1944b2ad116a6de760b27b4d874d173cb84
Aaron Langley <alangley345@gmail.com>
2022-04-10 10:27:16 -0400

    Removed Github Secrets tf file

--- terraform/gh_secrets.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "resume_distribution" {
-  repository       = "alangley345/cloudresume-frontend"
-  secret_name      = "DISTRIBUTION_ID"
-  encrypted_value  = "${aws_cloudfront_distribution.resume.id}"
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,21 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "github" {
-}
-
 #define location of state file
 terraform {
   backend "s3" {
     bucket = "myterraformcode"
     key    = "prod/cloudresume.tfstate"
     region = "us-east-1"
-  }
-
-    required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
   }
 }
