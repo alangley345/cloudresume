60017ce4b0b951489a7554455d72d6100a297079
Aaron Langley <alangley@gmail.com>
2022-04-18 10:15:54 -0400

    added local outputs for setting secrets for site content

--- terraform/localoutputs.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "bucket_name" {
-    content  = aws_s3_bucket.resume.id
-    filename = "s3bucketname.txt"
-}
-
-resource "local_file" "private_key" {
-    content  = aws_cloudfront_distribution.resume.id
-    filename = "cloudfrontdistribution.txt"
-}
