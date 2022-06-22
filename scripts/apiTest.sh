2948e5ef81ad90ba8b1c1f0686daf99377a7ff3f
Aaron Langley <alangley@gmail.com>
2022-06-22 11:28:37 -0400

    Checking the logic again, think this is working

--- scripts/apiTest.sh
+++ scripts/apiTest.sh
@@ -4,7 +4,7 @@ request1=$(curl https://api.aaronlangley.net/getCount | jq '.body.Item.visits')
 curl https://api.aaronlangley.net/updateCount
 request2=$(curl https://api.aaronlangley.net/getCount | jq '.body.Item.visits')
 
-if [[ $request1 > $request2 ]]
+if [[ $request2 > $request1 ]]
         then
             echo 'Cloudresume APIs are working'
             aws dynamodb update-item \
