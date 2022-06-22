c7016ae6cfd7926f126218f6d8bb1674ece200f3
Aaron Langley <alangley@gmail.com>
2022-06-22 10:41:20 -0400

    Commented out attribute values and hard coded increment

--- scripts/apiTest.sh
+++ scripts/apiTest.sh
@@ -10,9 +10,13 @@ if [[ $request1 > $request ]]
             aws dynamodb update-item \
             --table-name resume.aaronlangley.net \
             --key {\"event\":{\"S\":\"test\"}} \
-            --update-expression "set visits = visits - 1" \
-            --expression-attribute-names {\"N\":\"Visits\"} \
-            #--expression-attribute-values {\":inc\":\"1\"}
+            --update-expression "set visits = visits - :inc" \
+            --expression-attribute-names {\"S\":\"visits\"} \
+            --expression-attribute-values {\":inc\":{\"N\":\"1\"}}
         else
             echo 'Check your APIs again'
 fi
+
+#--update-expression 'SET #H = :h' \
+#--expression-attribute-names '{"#H":"Column_name_to_change"}' \
+#--expression-attribute-values '{":h":{"S":"Changed_Column_value"}}'
