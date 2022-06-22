e187332034806f11db7cf0e74509a3e0e2ea8629
Aaron Langley <alangley@gmail.com>
2022-06-21 23:46:17 -0400

    Added backend test and api test

---
+++ scripts/apiTest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+request1=$(curl https://api.aaronlangley.net/getCount | jq '.body.Item.visits')
+curl https://api.aaronlangley.net/updateCount
+request2=$(curl https://api.aaronlangley.net/getCount | jq '.body.Item.visits')
+
+if [[ $request1 > $request ]]
+        then
+            echo 'Cloudresume APIs are working'
+            aws dynamodb update-item \
+            --table-name resume.aaronlangley.net \
+            --key {'event': 'test',} \
+            --update-expression "set visits = visits - :inc"
+            --expression-attribute-values {
+                        ':inc':1
+                }
+            
+        else
+            echo 'Check your APIs again'
+fi
+
