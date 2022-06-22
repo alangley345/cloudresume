bc2caca20636b20f4355cd17af5fcc21043d96ce
Aaron Langley <alangley@gmail.com>
2022-06-22 11:09:49 -0400

    Commented out attribute values and hard coded increment

--- scripts/apiTest.sh
+++ scripts/apiTest.sh
@@ -11,7 +11,7 @@ if [[ $request1 > $request ]]
             --table-name resume.aaronlangley.net \
             --key {\"event\":{\"S\":\"test\"}} \
             --update-expression "set visits = visits + :inc" \
-            --expression-attribute-values {\":inc\":{\"N\":\"-1\"}}
+            --expression-attribute-values {\":inc\":{\"N\":\"-1\"}} \
             --expression-attribute-names {\"visits\":\"visits\"} \
             
         else
