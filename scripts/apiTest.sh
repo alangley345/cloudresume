d8436c9638dbb8b6dc985eeef7a2e898ac380f27
Aaron Langley <alangley@gmail.com>
2022-06-22 11:14:42 -0400

    Commented out attribute values and hard coded increment

--- scripts/apiTest.sh
+++ scripts/apiTest.sh
@@ -12,7 +12,7 @@ if [[ $request1 > $request ]]
             --key {\"event\":{\"S\":\"test\"}} \
             --update-expression "set visits = visits + :inc" \
             --expression-attribute-values {\":inc\":{\"N\":\"-1\"}} \
-            --expression-attribute-names {\"N\":\"visits\"} \
+            --expression-attribute-names {\"visits\"} \
             
         else
             echo 'Check your APIs again'
